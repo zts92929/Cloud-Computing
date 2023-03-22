95eb02e5a5426cfce6793826278119f51c0dac98
Zachary Smith <zts929@Zacharys-MacBook-Pro.local>
2023-03-22 11:56:14 -0600

    removed keys in all documents

--- ECE_4150_Lab3/aqi-django-app/env.py
+++ ECE_4150_Lab3/aqi-django-app/env.py
@@ -1,4 +1,4 @@
 # IAM User access configuration
-AWS_ACCESS_KEY="AKIAQAJDMJYGHTRP3KDK"
-AWS_SECRET_ACCESS_KEY="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"
-AWS_REGION="us-east-1"
+AWS_ACCESS_KEY="<insert access key>"
+AWS_SECRET_ACCESS_KEY="<insert secret access key>"
+AWS_REGION="<insert region>"
