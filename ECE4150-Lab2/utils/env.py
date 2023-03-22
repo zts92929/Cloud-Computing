e12e8551569d7727239f078b4053a42106f1733e
Zachary Smith <zts929@Zacharys-MacBook-Pro.local>
2023-03-22 11:53:50 -0600

    removed keys

--- ECE4150-Lab2/utils/env.py
+++ ECE4150-Lab2/utils/env.py
@@ -1,20 +1,20 @@
 # IAM User access configuration
-AWS_ACCESS_KEY="AKIAQAJDMJYGH6TAGXNO"
-AWS_SECRET_ACCESS_KEY="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"
-AWS_REGION="us-east-1"
+AWS_ACCESS_KEY="<Insert Access Key>"
+AWS_SECRET_ACCESS_KEY="<Insert Secret Access Key>"
+AWS_REGION="<Insert Region>"
 
 # S3 Bucket for photo objects
-PHOTOGALLERY_S3_BUCKET_NAME="photobucket-smith929-2021-4150"
+PHOTOGALLERY_S3_BUCKET_NAME="<Insert S3 bucket name>"
 
 # MySQL Configuration
-RDS_DB_HOSTNAME="photogallerydb.cud8yj6ejrno.us-east-1.rds.amazonaws.com"
-RDS_DB_USERNAME='root'
-RDS_DB_PASSWORD='Poopyboy21!'
-RDS_DB_NAME='photogallerydb'
+RDS_DB_HOSTNAME="<insert database name>"
+RDS_DB_USERNAME='<Insert DB username>'
+RDS_DB_PASSWORD='<Insert DB password>'
+RDS_DB_NAME='<Insert RDS database name'
 
 # DynamoDB Table
-DYNAMODB_TABLE='PhotoGallerydb'
-USER_DYNAMODB_TABLE='photogalleryuser'
+DYNAMODB_TABLE='<insert dynamodb table name>'
+USER_DYNAMODB_TABLE='<insert dynamodb user table name>'
 
 ###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
 
